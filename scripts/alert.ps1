deef6e16d83d339b707bc86c5acbabd8e4bfa633
Михаил <.email@gmail.com>
2026-01-27 12:30:10 +0300

    Add approve.sh

---
+++ scripts/alert.ps1
@@ -0,0 +1,2 @@
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("Antigravity: Требуется ваше подтверждение! Проверьте окна подтверждения команд в интерфейсе.", "Antigravity Alert", "OK", "Information")
